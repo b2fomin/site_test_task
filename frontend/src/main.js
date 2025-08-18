c7370eef845542ec9dc0f7cffe769a886bd93033
Nikita <fomin.nv@phystech.edu>
2025-08-18 20:44:28 +0300

    install needed packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import bootstrap from 'bootstrap/dist/js/bootstrap';
+window.bootstrap = bootstrap;
+
+import axios from 'axios';
+window.axios = axios;
+
 const app = createApp(App)
 
 app.use(router)
