d0117e9e7a0207ae94ec4f36caf1fa6a0a92dc62
Nikita <fomin.nv@phystech.edu>
2025-08-18 10:48:44 +0300

    for ide

--- backend/app/Models/Job.php
+++ backend/app/Models/Job.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperJob
+ */
 class Job extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
